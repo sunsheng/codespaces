5a8e8ad6780f3ca321adcc03ee22262b3ee7d0ac
sunsheng <sunsheng4214@gmail.com>
2022-05-07 02:12:11 +0000

    go

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 
 func main() {
 	fmt.Printf("hello\n")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(r.URL.Path))
+	})
+	http.ListenAndServe(":8080", nil)
 }
